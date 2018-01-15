4eb0a3c7b7f75ed3beadd2a3d5a6f9bf09a01f66
Ryan8765 <rmhaas2211@gmail.com>
2018-01-15 17:47:26 -0600

    Updated to include scss, config, components directory, removed files

--- src/App.js
+++ src/App.js
@@ -1,47 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import config from './config.js';
+
+// custom components
+import Test from './components/test/Test';
 
 
 class App extends Component {
 
-    constructor(props) {
-        super(props);
-
-
-        this.quickbase = new window.QuickBase({
-            realm: config.REALM,
-            userToken: config.USER_TOKEN,
-            appToken: config.APPLICATION_TOKEN
-        });
-
-
-    }
-
-    componentWillMount() {
-
-        this.quickbase.api('API_DoQuery', {
-            dbid: 'bkcdrryt2',
-            includeRids: true
-        }).then((results)=>{
-            alert("results in Quick Base " + JSON.stringify(results));
-        }).catch((err)=>{
-            console.log(err);
-        });
-
-    }
-
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Test />
       </div>
     );
   }
---
+++ src/components/test/Test.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import { Button } from 'reactstrap';
+import './test.css';
+import config from '../../config.js';
+
+
+
+
+class Test extends Component {
+
+    constructor(props) {
+        super(props);
+        this.quickbase = new window.QuickBase({
+            realm: config.REALM,
+            userToken: config.USER_TOKEN,
+            appToken: config.APPLICATION_TOKEN
+        });
+
+    }
+
+    componentWillMount() {
+
+        this.quickbase.api('API_DoQuery', {
+            dbid: 'bkcdrryt2',
+            includeRids: true
+        }).then((results)=>{
+            alert("results in Quick Base " + JSON.stringify(results));
+        }).catch((err)=>{
+            console.log(err);
+        });
+
+    }
+
+  render() {
+    return (
+      <div>
+        <h1 className="ryan-test">Testing some sass styling!!!</h1>
+        <Button color="danger">Danger!</Button>
+      </div>
+    );
+  }
+}
+
+export default Test;
